f9f8ebe469ad022b0c5a49ea2d28d5e16d799767
vishakhas1 <99073862+vishakhas1@users.noreply.github.com>
2022-04-05 12:11:04 +0530

    assisted

---
+++ assisted projects/FeedbackApplication.java
@@ -0,0 +1,8 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class FeedbackApplication {
+public static void main(String[] args) {
+SpringApplication.run(FeedbackApplication.class, args);
+}
+}
